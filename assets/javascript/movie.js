c35f95a628077fc8120e583a18ba46534053e45c
Anita Brument <Anita@Anitas-MacBook-Pro.local>
2017-08-02 22:20:22 -0700

    init

---
+++ assets/javascript/movie.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	// show modal on page load
+	$("#signInModal").modal('show');
+
+});
+
+
+// submit button for user keywords
+$("#user-keyword-btn").on("click", function(event){
+
+	// prevent page from reloading when clicking on submit button
+	event.preventDefault();
+
+});
